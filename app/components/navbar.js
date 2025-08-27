0bb2864d359b47b0f3dcec70b9868d8cabf13aa0
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-08-27 21:28:45 +0000

    Refined navbar layout

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function Navbar() {
   return (
     <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 p-5">
-      <nav className="flex items-center justify-evenly w-80 md:w-96 px-6 py-5 rounded-full shadow-md background backdrop-blur-md border border-dark-gray">
+      <nav className="flex items-center justify-evenly w-80 sm:120 md:w-150 max-w-screen px-6 py-4 rounded-full shadow-md background backdrop-blur-md border border-dark-gray">
         {/* Left - Navigation */}
         <div>
           Navigation
