c16fb16590d14a049c28add682ce3aa9ada29226
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-08-27 20:01:13 +0000

    Changed navbar to preferred style

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,15 +1,20 @@
+"use client";
+import { useState } from "react";
+
 export default function Navbar() {
   return (
-    <nav className="border border-red-200 w-full bg-background text-foreground px-6 py-4 flex items-center justify-between">
-      {/* Logo / Name */}
-      <div className="text-xl font-bold">Temp navbar</div>
+    <div className="fixed top-4 left-1/2 -translate-x-1/2 z-50 p-5">
+      <nav className="flex items-center justify-evenly w-80 md:w-96 px-6 py-5 rounded-full shadow-md background backdrop-blur-md border border-dark-gray">
+        {/* Left - Navigation */}
+        <div>
+          Navigation
+        </div>
 
-      {/* Links */}
-      <ul className="flex space-x-6 text-sm font-medium">
-        <li><a href="#about" className="hover:text-red-400">About</a></li>
-        <li><a href="#projects" className="hover:text-gray-400">Projects</a></li>
-        <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
-      </ul>
-    </nav>
+        {/* Center - Social */}
+        <div>
+          Social
+        </div>
+      </nav>
+    </div>
   );
 }
