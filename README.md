227e9882d9410e7aaa17028dd284ed2b8533a77c
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-08-30 23:13:31 +0000

    Continued work on Experience section

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Personal Digital Portfolio
 
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+Tech Stack
+- Next.js
+- Tailwind CSS
+- React Bits (TBC)
+- Magix UI (TBC)
--- app/components/experience.js
+++ app/components/experience.js
@@ -1,30 +1,19 @@
 export default function Experience() {
   return (
-    <section className="bg-red-200 w-full py-10 px-1">
-      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold">Experience</h1>
-      <div className="flex flex-col max-w-4xl gap-6">
-        
-        {/* Subtitle / description */}
-        <p className="text-lg sm:text-xl max-w-xl">
-          I build modern, responsive websites and web apps using Next.js and TailwindCSS.
-        </p>
+    <section className="flex flex-col w-full py-10 gap-6">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold pt-10 pb-2">Experience</h1>
 
-        {/* Call to action buttons */}
-        <div className="flex flex-col sm:flex-row gap-4 mt-4">
-          <a
-            href="#projects"
-            className="py-1 px-3 bg-sky-600 text-white rounded-lg font-medium hover:bg-sky-700 transition"
-          >
-            Download CV
-          </a>
-          <a
-            href="#contact"
-            className="py-1 px-3 border border-sky-600 text-sky-600 rounded-lg font-medium hover:bg-sky-700 transition"
-          >
-            Contact Me
-          </a>
-        </div>
+      <div className="px-0 sm:px-5 md:px-25">
+        <h2 className="text-lg font-bold">Insync Insurance</h2>
+        <p className="py-1 italic text-zinc-400">Operations Technician [02/2024 – present]</p>
+        <ul className="list-disc list-outside px-8">
+          <li>As an Operations Technician, I handle a range of tasks, including daily API user policy uploads and supporting the Accounts & Credit Control teams with refund requests and shortfall claims.</li>
+          <li>A significant part of my role is to optimise internal processes and develop the supporting process guides, such as streamlining the New Starter & Leaver process for managers, Operations team and third party companies.</li>
+          <li>I also serve as a fire warden, managing emergency resources like floor plans.</li>
+          <li> Additionally, I collaborate with my manager to refine Acturis configuration and templates, ensuring SMS, email, and document templates are consistent, cost-effective, and compliant.</li>
+        </ul>
       </div>
+
     </section>
   );
 }
