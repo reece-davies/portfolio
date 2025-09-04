8092a039e022e3d994b917d84f751cd49354def8
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-09-04 21:43:06 +0000

    Highlighted specific words

--- app/components/experience.js
+++ app/components/experience.js
@@ -10,7 +10,7 @@ export default function Experience() {
           <li>As an Operations Technician, I handle a range of tasks, including daily API user policy uploads and supporting the Accounts & Credit Control teams with refund requests and shortfall claims.</li>
           <li>A significant part of my role is to optimise internal processes and develop the supporting process guides, such as streamlining the New Starter & Leaver process for managers, Operations team and third party companies.</li>
           <li>I also serve as a fire warden, managing emergency resources like floor plans.</li>
-          <li> Additionally, I collaborate with my manager to refine Acturis configuration and templates, ensuring SMS, email, and document templates are consistent, cost-effective, and compliant.</li>
+          <li> Additionally, I collaborate with my manager to refine <span className="text-sky-500 hover:text-sky-600 font-medium">Acturis</ span> configuration and templates, ensuring SMS, email, and document templates are consistent, cost-effective, and compliant.</li>
         </ul>
       </div>
 
@@ -22,7 +22,7 @@ export default function Experience() {
           <li>After working for ETA for two years, I was appointed as Head of Marketing in September 2022 based on my prior experience and education in marketing from my MSc.</li>
           <li>From this position, I was responsible for managing the club's social media presence, producing monthly newsletters, and coordinating public relations efforts.</li>
           <li> In addition to my coaching duties, I successfully implemented a range of marketing strategies that helped increase brand awareness and attract new customers to the club.</li>
-          <li> I also served as tech-support for ETA, specifically in regard to the deployment and management of SafeGaze, a software I developed for ETA in my final year of my BSc</li>
+          <li> I also served as tech-support for ETA, specifically in regard to the deployment and management of <span className="text-sky-500 hover:text-sky-600 font-medium">SafeGaze</ span>, a software I developed for ETA in my final year of my BSc</li>
         </ul>
       </div>
 
@@ -32,7 +32,7 @@ export default function Experience() {
         <ul className="list-disc list-outside px-8">
           <li>I was awarded an Industrial Placement within the Met Office (Exeter). During this time, I worked as a cyber security analyst in the Cyber Security Operations Centre (CSOC).</li>
           <li>My team and I were responsible for the logging and monitoring of the security of Met Office’s internal systems.</li>
-          <li>Additionally, I was involved in the deployment of The Logging, Monitoring and Alerting project (LMA Project) which makes use of Elastic Stack’s detection tools and resources for identifying malicious activity from within the organisation.</li>
+          <li>Additionally, I was involved in the deployment of <span className="text-sky-500 hover:text-sky-600 font-medium">The Logging, Monitoring and Alerting project (LMA Project) </ span> which makes use of Elastic Stack’s detection tools and resources for identifying malicious activity from within the organisation.</li>
           <li>My experience at the Met Office allowed me to develop as a professional and provided me with a deeper understanding of how to work collaboratively with a team in a technology-focused role.</li>
         </ul>
       </div>
