328904dcf7b362e83d164504506b078d2652deed
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-09-07 17:06:48 +0000

    Changed DotGrid to span across entire page

--- app/page.js
+++ app/page.js
@@ -6,29 +6,30 @@ import DotGrid from "./components/reactbits/DotGrid"
 
 export default function Home() {
   return (
-    <div className="font-sans flex flex-col min-h-screen gap-16 sm:gap-24 px-3 sm:px-25">
-      <main className="flex flex-col items-start">
+    <div className="font-sans flex flex-col min-h-screen">
+      <section className="relative w-full flex">
+        <div
+          style={{ width: '100vw', height: '100vh', position: 'absolute', left: '0', top: '0' }}
+          className="-z-10">
+          <DotGrid
+            dotSize={10}
+            gap={15}
+            baseColor="#1a1a1a"
+            activeColor="#0ea5e9"
+            proximity={120}
+            shockRadius={250}
+            shockStrength={5}
+            resistance={750}
+            returnDuration={1.5}
+          />
+        </div>
 
-        <section className="relative w-full flex">
-          <div
-            style={{ width: '100%', height: '600px', position: 'absolute' }}
-            className="-z-10">
-            <DotGrid
-              dotSize={10}
-              gap={15}
-              baseColor="#2E2E2E"
-              activeColor="#0ea5e9"
-              proximity={120}
-              shockRadius={250}
-              shockStrength={5}
-              resistance={750}
-              returnDuration={1.5}
-            />
-          </div>
-
+        <div className="w-full px-3 sm:px-25">
           <Hero />
-        </section>
+        </div>
+      </section>
 
+      <main className="flex flex-col items-start px-3 sm:px-25 gap-2">
         <Experience />
         <Skills />
       </main>
