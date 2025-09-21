b0e4ea7628b1d2ed4eb225ee8d13af0b667ce10a
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-09-21 00:42:10 +0000

    Personalised footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -2,13 +2,8 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center pb-15">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center pt-10 pb-15">
+        <a className="flex items-center justify-center p-1 gap-2 text-xl">
           <Image
             aria-hidden
             src="/file.svg"
@@ -16,37 +11,7 @@ export default function Footer() {
             width={16}
             height={16}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Reece Davies
         </a>
       </footer>
   );
